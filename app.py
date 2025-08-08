2efabf8675e5b3ea25e0b2ddbff2f2656208f115
Nickolas713 <146655458+Nickolas713@users.noreply.github.com>
2025-08-08 16:49:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -93,6 +93,6 @@ def convert_tables_to_csv():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=7001)
 
 
